0d56729d0727d7f35a00739bd1bf465517e59916
ChrysanthosM <ChrysanthosM@Outlook.com>
2025-11-23 11:41:19 +0200

    Commit

--- src/main/java/org/masouras/data/service/FileOnDBActions.java
+++ src/main/java/org/masouras/data/service/FileOnDBActions.java
@@ -1,12 +1,18 @@
 package org.masouras.data.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.masouras.printing.sqlite.repo.squad.data.ActivityJ2SQL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class FileOnDBActions {
+    private final ActivityJ2SQL activityJ2SQL;
 
-
+    @Autowired
+    public FileOnDBActions(ActivityJ2SQL activityJ2SQL) {
+        this.activityJ2SQL = activityJ2SQL;
+    }
 
 }
