56c565cc72306acf7953add092507efdcc4a47a1
ChrysanthosM <ChrysanthosM@Outlook.com>
2025-11-26 20:12:48 +0200

    Commit

--- src/main/java/org/masouras/data/service/FileOnDBActions.java
+++ src/main/java/org/masouras/data/service/FileOnDBActions.java
@@ -1,10 +1,14 @@
 package org.masouras.data.service;
 
+import jakarta.persistence.Query;
 import lombok.extern.slf4j.Slf4j;
 import org.masouras.printing.sqlite.repo.squad.data.ActivityJ2SQL;
 import org.masouras.printing.sqlite.repo.squad.data.ActivityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -16,10 +20,14 @@ public class FileOnDBActions {
         this.activityJ2SQL = activityJ2SQL;
     }
 
+    @Transactional
     public int createActivity() {
-        String runSql = activityJ2SQL.getSQL(ActivityRepo.NameOfSQL.INSERT);
-        if (log.isDebugEnabled()) log.debug("Will run sql: {}", runSql);
-        return 0;
+        Query runQuery = activityJ2SQL.getNativeQuery(ActivityRepo.NameOfSQL.INSERT);
+        runQuery.setParameter(1, "10001");
+        runQuery.setParameter(2, this.getClass().getName());
+        runQuery.setParameter(3, System.getProperty("user.name"));
+        runQuery.setParameter(4, LocalDateTime.now());
+        return runQuery.executeUpdate();
     }
 
 }
