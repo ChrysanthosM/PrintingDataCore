ba4ceab47a8d9bfe472f2a7d0807dd50b79a7005
ChrysanthosM <ChrysanthosM@Outlook.com>
2025-11-27 19:04:39 +0200

    Commit

--- src/main/java/org/masouras/data/service/FileOnDBActions.java
+++ src/main/java/org/masouras/data/service/FileOnDBActions.java
@@ -1,9 +1,8 @@
 package org.masouras.data.service;
 
-import jakarta.persistence.Query;
 import lombok.extern.slf4j.Slf4j;
-import org.masouras.printing.sqlite.repo.squad.data.ActivityJ2SQL;
-import org.masouras.printing.sqlite.repo.squad.data.ActivityRepo;
+import org.masouras.printing.sqlite.schema.entity.ActivityEntity;
+import org.masouras.printing.sqlite.schema.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,21 +12,22 @@ import java.time.LocalDateTime;
 @Service
 @Slf4j
 public class FileOnDBActions {
-    private final ActivityJ2SQL activityJ2SQL;
+    private final ActivityRepository activityRepository;
 
     @Autowired
-    public FileOnDBActions(ActivityJ2SQL activityJ2SQL) {
-        this.activityJ2SQL = activityJ2SQL;
+    public FileOnDBActions(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
     }
 
     @Transactional
-    public int createActivity() {
-        Query runQuery = activityJ2SQL.getNativeQuery(ActivityRepo.NameOfSQL.INSERT);
-        runQuery.setParameter(1, "10001");
-        runQuery.setParameter(2, this.getClass().getName());
-        runQuery.setParameter(3, System.getProperty("user.name"));
-        runQuery.setParameter(4, LocalDateTime.now());
-        return runQuery.executeUpdate();
-    }
+    public Long createActivity() {
+        ActivityEntity activity = new ActivityEntity();
+        activity.setActivityType("10001");
+        activity.setPgmStamp(this.getClass().getName());
+        activity.setUserStamp(System.getProperty("user.name"));
+        activity.setDateStamp(LocalDateTime.now());
 
+        return activityRepository.save(activity).getId();
+    }
 }
+
