e43804f4d4829d247d59e5c6577621bea55f839a
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-01-01 16:03:34 +0200

    commit

--- src/main/java/org/masouras/data/control/converter/FileOkAdapter.java
+++ src/main/java/org/masouras/data/control/converter/FileOkAdapter.java
@@ -6,14 +6,15 @@ import org.masouras.data.domain.FileOkRaw;
 import org.masouras.model.mssql.schema.jpa.control.entity.enums.ActivityType;
 import org.masouras.model.mssql.schema.jpa.control.entity.enums.ContentType;
 import org.masouras.model.mssql.schema.jpa.control.entity.enums.FileExtensionType;
+import org.masouras.model.mssql.schema.jpa.control.util.EnumUtil;
 
 @UtilityClass
 public class FileOkAdapter {
     public static FileOkDto toFileOkDto(FileOkRaw fileOkRaw) {
         return new FileOkDto(
-                FileExtensionType.getFromCode(fileOkRaw.getFileExtension()),
-                ContentType.getFromCode(fileOkRaw.getContentType()),
-                ActivityType.getFromCode(fileOkRaw.getActivityType())
+                EnumUtil.fromCode(FileExtensionType.class, fileOkRaw.getFileExtension()),
+                EnumUtil.fromCode(ContentType.class, fileOkRaw.getContentType()),
+                EnumUtil.fromCode(ActivityType.class, fileOkRaw.getActivityType())
         );
     }
 }
