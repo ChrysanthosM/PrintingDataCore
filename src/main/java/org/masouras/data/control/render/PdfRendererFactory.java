35342cc940989d6e8145d79bac6f32237c6e2b19
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-01-21 19:09:09 +0200

    commit

--- src/main/java/org/masouras/data/control/render/PdfRendererFactory.java
+++ src/main/java/org/masouras/data/control/render/PdfRendererFactory.java
@@ -1,21 +1,29 @@
 package org.masouras.data.control.render;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.masouras.model.mssql.schema.jpa.control.entity.enums.RendererType;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class PdfRendererFactory {
-    private final Map<RendererType, PdfRenderer> renderers;
+    private final List<PdfRenderer> pdfRenderers;
+    private final Map<RendererType, PdfRenderer> pdfRendererMap;
 
     public PdfRenderer getRenderer(RendererType rendererType) {
-        PdfRenderer renderer = renderers.getOrDefault(rendererType,null);
+        PdfRenderer renderer = pdfRendererMap.getOrDefault(rendererType,null);
         if (renderer == null) {
-            throw new IllegalArgumentException("Unknown renderer: " + rendererType);
+            throw new IllegalArgumentException("Unknown renderer: " + rendererType.name());
         }
         return renderer;
+    }
+
+    @PostConstruct
+    private void init() {
+        pdfRenderers.forEach(fv -> pdfRendererMap.put(fv.getPdfRendererType(), fv));
     }
 }
