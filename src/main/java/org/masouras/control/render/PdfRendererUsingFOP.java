6205d021998f7432803a007934b648a5ef7f9387
ChrysanthosM <ChrysanthosM@Outlook.com>
2026-03-17 15:27:53 +0200

    commit

--- src/main/java/org/masouras/control/render/PdfRendererUsingFOP.java
+++ src/main/java/org/masouras/control/render/PdfRendererUsingFOP.java
@@ -1,8 +1,10 @@
 package org.masouras.control.render;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
+import org.apache.fop.configuration.Configuration;
 import org.apache.xmlgraphics.util.MimeConstants;
 import org.masouras.model.mssql.schema.jpa.control.entity.enums.RendererType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @Component
 public non-sealed class PdfRendererUsingFOP implements PdfRenderer {
     private static final String FOP_CONFIG_XML = """
@@ -37,7 +40,9 @@ public non-sealed class PdfRendererUsingFOP implements PdfRenderer {
 
     @Autowired
     public PdfRendererUsingFOP() throws Exception {
-        this.fopFactory = FopFactory.newInstance(new File(".").toURI(), new ByteArrayInputStream(FOP_CONFIG_XML_BYTES));
+        fopFactory = FopFactory.newInstance(new File(".").toURI(), new ByteArrayInputStream(FOP_CONFIG_XML_BYTES));
+        Configuration config = fopFactory.getUserConfig();
+        if (log.isInfoEnabled()) log.info("FOP Configuration loaded: {}", config);
     }
 
     @Override
@@ -51,7 +56,10 @@ public non-sealed class PdfRendererUsingFOP implements PdfRenderer {
              ByteArrayOutputStream pdfOut = new ByteArrayOutputStream()) {
 
             FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, pdfOut);
+            Fop fop;
+            synchronized (fopFactory) {
+                fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, pdfOut);
+            }
 
             Transformer transformer = templates.newTransformer();
             transformer.transform(
